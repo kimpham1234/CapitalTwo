f15041f8eb6facc46887d23a051f690273b29b58
haeol <k.tom_1@yahoo.com>
2017-11-21 12:56:40 -0800

    added native query functionality

--- src/main/java/capitaltwo/repository/CustomerAccountRepository.java
+++ src/main/java/capitaltwo/repository/CustomerAccountRepository.java
@@ -2,11 +2,16 @@ package capitaltwo;
 
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import java.util.List;
 
 
 // This will be AUTO IMPLEMENTED by Spring into a Bean called userRepository
 // CRUD refers Create, Read, Update, Delete
 
 public interface CustomerAccountRepository extends BaseAccountRepository<CustomerAccount> {
+
+    @Query(value="SELECT * from customer_account", nativeQuery=true)
+    List<Object> getCustomers();
 
 }
